1495238698821d9c661cb6c5b5dd194098d06233
Joakim Rydberg <lr.joakim.rydberg@gmail.com>
2018-06-24 21:43:46 +0200

    Player can restart game after defeat.

--- game_functions.py
+++ game_functions.py
@@ -35,7 +35,7 @@ def check_keyup_events(event, ship):
 		ship.moving_left = False
 
 
-def check_events(ai_settings, screen, stats, play_button, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
 	"""Responds to keypresses and mouse events."""
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -47,13 +47,24 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets):
 			check_keyup_events(event, ship)
 		elif event.type == pygame.MOUSEBUTTONDOWN:
 			mouse_x, mouse_y = pygame.mouse.get_pos()
-			check_play_button(stats, play_button, mouse_x, mouse_y)
+			check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
 
 
-def check_play_button(stats, play_button, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
 	"""Start a new game when the player clicks Play."""
-	if play_button.rect.collidepoint(mouse_x, mouse_y):
+	button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+	if button_clicked and not stats.game_active:
+		# Reset the game statistics.
+		stats.reset_stats()
 		stats.game_active = True
+
+		# Empty the list of aliens and bullets.
+		aliens.empty()
+		bullets.empty()
+
+		# Create a new fleet and centre the ship.
+		create_fleet(ai_settings, screen, ship, aliens)
+		ship.centre_ship()
 
 
 def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
